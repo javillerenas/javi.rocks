0a398fea9e9d1b13a5d147bf8cf5e786616a382c
Javi Llerenas <javr.llerenas@gmail.com>
2021-11-13 09:20:20 -0800

    fix: import GA_TRACKING_ID from the correct file

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,7 +5,7 @@ import Document, {
   Main,
   NextScript,
 } from 'next/document';
-import { GA_TRACKING_ID } from '../lib/gtag';
+import { GA_TRACKING_ID } from 'helpers/constants';
 
 class MyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
